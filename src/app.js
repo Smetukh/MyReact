f336166ed14a31aa12aa4ebb11bbc11505b298d8
Alex Smetukh <alex.smetukh@gmail.com>
2019-04-24 23:12:51 +0300

    finish my react
    finish my react

--- src/app.js
+++ src/app.js
@@ -1,62 +1,10 @@
-function anElement(element, children) {
-  if (typeof element === "function") {
-    return element();
-  } else {
-    const anElement = document.createElement(element);
-    children.forEach(child => {
-      if (typeof child === "object") {
-        anElement.appendChild(child);
-      } else {
-        anElement.innerHTML += child;
-      }
-    });
-    return anElement;
-  }
-}
+import React from "./react";
 
-function createElement(el, props, ...children) {
-  return anElement(el, props, children);
-}
-// function render (el, domEl) {
-//   return domEl.appendChild(el);
-// }
+const app = React.createElement("div", { style: { backgroundColor: "red" } }, [
+  React.createElement("span", undefined, ["Hello world"]),
+  React.createElement("br"),
+  "This is just a text node",
+  React.createElement("div", { textContent: "Text content" })
+]);
 
-const React = {
-  createElement,
-  render: (el, domEl) => {
-    domEl.appendChild(el);
-  }
-};
-
-// const helloWorld =
-//   React.createElement('div', { style: { backgroundColor: 'red' } }
-// , [
-//   React.createElement('span', undefined, 'Hello world'),
-//   React.createElement('br'),
-//   'This is just a text node',
-//   React.createElement('div', { textContent: 'Text content' }),
-// ]
-// );
-// const helloWorld = React.createElement('div', null, `Hello World`);
-const Hello = function() {
-  return React.createElement("div", null, `Hello World`);
-};
-const helloWorld = React.createElement(Hello, null, null);
-const helloWorld2 = React.createElement(Hello, null, null);
-const regularDiv = React.createElement("div", null, `I'm just a regular div`);
-
-const parent = React.createElement(
-  "div",
-  null,
-  helloWorld,
-  helloWorld2,
-  regularDiv,
-  ` I'm just a text`
-);
-
-ReactDOM.render(parent, document.getElementById("root"));
-
-// React.render(
-//   app,
-//   document.getElementById('root'),
-// );
+React.render(app, document.getElementById("app"));
--- src/react.js
+++ src/react.js
@@ -1,20 +1,54 @@
-(() => {
-  function anElement(element, children) {
+const React = {
+  createElement,
+  render
+};
+function createElement(el, props, children) {
+  return anElement(el, props, children);
+}
+function render(el, domEl) {
+  return domEl.appendChild(el);
+}
+
+function camelToDashString(str) {
+  return str.replace(/[A-Z]/g, m => "-" + m.toLowerCase());
+}
+//convert style from camelCase to dash-case
+function styleToString(style) {
+  return Object.entries(style).reduce((styleString, [propName, propValue]) => {
+    return `${styleString}${camelToDashString(propName)}:${propValue};`;
+  }, "");
+}
+
+// append every child in array
+function anElement(element, props, children) {
+  console.log("element = ", element);
+  console.log("children = ", children);
+  if (typeof element === "function") {
+    return element();
+  } else {
     const anElement = document.createElement(element);
-    anElement.innerHTML = children.join(" ");
+
+    //check for style props and convert them from camelCase to dash-case
+    props &&
+      Object.keys(props).forEach(propName => {
+        anElement[propName] =
+          propName === "style"
+            ? styleToString(props[propName])
+            : props[propName];
+      });
+    //append elements
+    if (Array.isArray(children)) {
+      children.forEach(child => {
+        if (typeof child === "object") {
+          anElement.appendChild(child);
+        } else {
+          anElement.innerHTML += child;
+        }
+      });
+    } else if (typeof children === "object") {
+      anElement.appendChild(children);
+    }
     return anElement;
   }
-
-  function createElement(el, props, ...children) {
-    return anElement(el, children);
-  }
-
-  window.React = {
-    createElement
-  };
-  window.ReactDOM = {
-    render: (el, domEl) => {
-      domEl.appendChild(el);
-    }
-  };
-})();
+}
+export default React;
